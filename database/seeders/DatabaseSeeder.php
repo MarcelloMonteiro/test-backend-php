9d102f3c4c3e27ab52a10cf67c1ab96ab77a309a
marcellomonteiro <marcelodjmix2013@gmail.com>
2023-02-21 21:57:28 -0300

    implements auth jwt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        \App\Models\Employee::create([
+            'name' => 'Usuário de teste',
+            'email' => 'teste@teste.com.br',
+            'password' => bcrypt( 'Senh@123' )
+        ]);
     }
 }
